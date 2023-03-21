f863e9bb8de43812814a58da60e570fda93b58b2
Debora <amaraldeboradsa@gmail.com>
2023-03-21 11:35:54 -0300

    Exercícios JavaScript

---
+++ script.js
@@ -0,0 +1,12 @@
+// Converter de float para int
+let numberFloat = 30.345
+console.log(parseInt(numberFloat))
+
+let numberFloat02 = 154.666
+console.log(parseInt(numberFloat02))
+
+let numberFloat03 = 50.888
+console.log(parseInt(numberFloat03))
+
+let numberFloat04 = 089.52
+console.log(parseInt(numberFloat04))
