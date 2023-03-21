3d258acf38f8820e55aabef0da0fa00cc66d97e1
Debora <amaraldeboradsa@gmail.com>
2023-03-21 11:44:26 -0300

    Exercícios JavaScript

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ console.log(parseInt(numberFloat03))
 
 let numberFloat04 = 089.52
 console.log(parseInt(numberFloat04))
+
+// Converter de string para float
+let convertFloat = "30.345"
+console.log(parseFloat(convertFloat))
+
+let convertFloat02 = "154.666"
+console.log(parseFloat(convertFloat02))
+
+let convertFloat03 = "50.888"
+console.log(parseFloat(convertFloat03))
+
+let convertFloat04 = "089.52"
+console.log(parseFloat(convertFloat04))
+
+// Tamanho das frases
+let numberString = "A MAIOR TECNOLOGIA"
+console.log(numberString.length)
+
+let numberString02 = "HARDWARE FAZ O COMPUTADOR"
+console.log(numberString02.length)
+
+let numberString03 = "FIAP A MELHOR FACULDADE"
+console.log(numberString03.length)
+
+let numberString04 = "TENCOLOGIA E INOVAÇÃO"
+console.log(numberString04.length)
